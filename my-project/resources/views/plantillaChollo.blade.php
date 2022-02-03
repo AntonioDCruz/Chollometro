8749465859579d3c581871275d1378287c346b0d
AntonioDCruz <andavido150@gmail.com>
2022-02-03 17:14:23 +0100

    creo que ya funciona auth

--- my-project/resources/views/plantillaChollo.blade.php
+++ my-project/resources/views/plantillaChollo.blade.php
@@ -12,6 +12,9 @@ $year = substr($mytime->toDateTimeString(), 0, 4);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CholloSevero</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" type="text/css" >
 </head>
 
@@ -33,6 +36,37 @@ $year = substr($mytime->toDateTimeString(), 0, 4);
             <li class="nav-item">
                 <a class="nav-link text-white" href="{{ route('destacados') }}">Destacados</a>
             </li>
+            @guest
+              @if (Route::has('login'))
+                  <li class="nav-item">
+                      <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
+                  </li>
+              @endif
+
+              @if (Route::has('register'))
+                  <li class="nav-item">
+                      <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
+                  </li>
+              @endif
+              @else
+              <li class="nav-item dropdown">
+                  <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                      {{ Auth::user()->name }}
+                  </a>
+
+                  <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                      document.getElementById('logout-form').submit();">
+                          {{ __('Logout') }}
+                      </a>
+
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                          @csrf
+                      </form>
+                  </div>
+              </li>
+            @endguest            
           </ul>
           <button class="btn btn-sm" id="btnCrearChollo" onclick="window.location.href='{{ route('creaChollo') }}'">Crear Chollo</button>
         </div>
--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PagesController;
 use GuzzleHttp\Middleware;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,16 +25,17 @@ use Illuminate\Support\Facades\Route;
 Route::view('nuevos', 'nuevos')->name('nuevos');
 Route::view('destacados', 'destacados')->name('destacados');*/
 
+Auth::routes();
+
 Route::get('/', [ PagesController::class, 'inicio'])->name('inicio');
 Route::get('nuevos', [ PagesController::class, 'nuevos' ])->name('nuevos');
 Route::get('destacados', [ PagesController::class, 'destacados' ])->name('destacados');
-Route::get('creaChollo', [ PagesController::class, 'creaChollo' ])->name('creaChollo');
+Route::get('creaChollo', [ HomeController::class, 'creaChollo' ])->name('creaChollo');
 Route::post('chollos', [ PagesController::class, 'crear' ]) -> name('chollos.crear');
 
-Route::get('login', [ PagesController::class, 'login'])->name('login');
-Route::get('register', [ PagesController::class, 'register'])->name('register');
+
 //Route::get('editaChollo/{id}', [ PagesController::class, 'editar' ]) -> name('editaChollo')->middleware('auth');
-Route::middleware('auth')->get('editaChollo/{id}', [ PagesController::class, 'editar' ]) -> name('editaChollo');
+Route::get('editaChollo/{id}', [ HomeController::class, 'editar' ]) -> name('editaChollo');
 Route::put('editaChollo/{id}', [ PagesController::class, 'actualizar' ]) -> name('actualizarChollo');
 
 Route::delete('eliminarChollo/{id}', [ PagesController::class, 'eliminar' ]) -> name('eliminarChollo');
@@ -40,6 +43,4 @@ Route::delete('eliminarChollo/{id}', [ PagesController::class, 'eliminar' ]) -> 
 Route::get('vistaChollo/{id}', [ PagesController::class, 'vistaChollo' ])->name('vistaChollo');
 
 //Route::get('/', [ PagesController::class, 'chollos' ]);
-//Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
